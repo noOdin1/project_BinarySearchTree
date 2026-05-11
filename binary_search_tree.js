af080bd50ccdb16a2cbaaa70846b1ea3c84afbb5
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-11 17:19:52 +0800

    Boiler plate for both source js and test js

---
+++ binary_search_tree.js
@@ -0,0 +1,11 @@
+/* binary_search_tree.js */
+
+/* class definition for a node point for this project */
+class Node {
+  constructor(val = null, nextNode = null) {
+    // Initial conditions of a node point
+    this.val = val;
+    this.leftNode = null;
+    this.rightNode = null;
+  }
+}
