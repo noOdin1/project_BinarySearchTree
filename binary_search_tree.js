1bf1f7b57900be689794ccc1e6c79fe7cf8c98c4
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-14 23:04:33 +0800

    Function from TOP to print out the binary tree, refitted to work

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -14,6 +14,25 @@ class Tree {
   constructor(rootNode = null) {
     this.rootNode = rootNode;
   }
+
+  prettyPrint(node = this.rootNode, prefix = "", isLeft = true) {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.prettyPrint(
+      node.rightNode,
+      `${prefix}${isLeft ? "│   " : "    "}`,
+      false,
+    );
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.val}`);
+    this.prettyPrint(
+      node.leftNode,
+      `${prefix}${isLeft ? "    " : "│   "}`,
+      true,
+    );
+  }
+
 }
 
 export { Node, Tree };
